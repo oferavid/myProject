291c04c0b446ee65d6ecad8cd61caf10e7b73960
Ofer <o.r.tivon@gmail.com>
2023-10-12 08:58:46 +1100

    Going to push this change to origin v01

--- main.py
+++ main.py
@@ -2,9 +2,10 @@
 
 def main(msg):
     print(msg)
-# This is a diff comment from pi2 dir- merge conflict created
-# This is  a comment from main.py pi dir
-# This is a new comment pushed from pycharm version control.
+# This is no longer needed.
+
+def secondary():
+    print("This is a new function")
 
 main("Hello Friends this is my pi2 directory main.py file")
 
