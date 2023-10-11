36b63d4b0bffe1763e2917517e9f7bbeb21f7f27
Ofer <o.r.tivon@gmail.com>
2023-10-12 09:15:39 +1100

    Going to push this change to origin v02

--- main.py
+++ main.py
@@ -7,9 +7,13 @@ def main(msg):
 def secondary():
     print("This is a new function")
 
-def thirdFunc()
+def thirdFunc():
     print ("This relevant to vo2 branch")
 
+def forthFunc(param):
+    print(param)
+
+forthFunc("Hey! this is just a practice.")
 main("Hello Friends this is my pi2 directory main.py file")
 
 
