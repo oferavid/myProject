b62476365dc009a809d94d22003b5a58ec0788c3
Ofer <o.r.tivon@gmail.com>
2023-10-12 08:46:51 +1100

    This is gonna work now- push from pycharm version control

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 practicing push and pull, commit, etc.
 
 This is a new comment from pycharm in ubuntu.
+
+This is a new change, push from pycharm version control.
--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 def main(msg):
     print(msg)
 # This is a diff comment from pi2 dir- merge conflict created
-#This is  a comment from main.py pi dir
+# This is  a comment from main.py pi dir
+# This is a new comment pushed from pycharm version control.
 
 main("Hello Friends this is my pi2 directory main.py file")
 
